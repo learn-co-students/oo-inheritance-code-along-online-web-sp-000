8a0ee77011fa9c42145bcd570ecad61d3cee12b4
Ashley Miller <asm2fe@virginia.edu>
2019-04-22 20:20:41 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -2,7 +2,7 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
 
-  attr_accessor :size, :wheel_size, :wheel_number
+  #attr_accessor :size, :wheel_size, :wheel_number
 
   def initialize(size, number)
     @wheel_size = size

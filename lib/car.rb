fcc4d3cd8e5ca46fd0b56182e489a67afcb5ace2
Ramon Chavez <rachavez1974@gmail.com>
2019-05-20 15:52:53 -0400

    Finish oo-inheritance-code-along-online-web-sp-000

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,17 @@
 require_relative "./vehicle.rb"
+  
+class Car < Vehicle
+
+  def initialize(wheel_size, number)
+    super(wheel_size, number)
+  end
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+  def fill_up_tank
+    super
+  end
+
+end

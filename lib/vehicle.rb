56128dda9eb3468c2eb32c9cd6ba64f89faf3d0f
remote-torrent-6747 <abbyc@sky.com>
2021-01-26 20:29:02 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,19 @@
-class Vehicle
+class Vehicle #Super Class
+  attr_accessor :wheel_size, :wheel_number
 
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  #initialize with a wheel size and number
+
+  def go
+    "vrrrrrrrooom!"
+  end
+  #common vehicle behaviour
+
+  def fill_up_tank
+    "filling up!"
+  end
+  #common vehicle behaviour
 end
